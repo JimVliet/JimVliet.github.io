de0428f84e4b8e48a6d80285d065d10f41004557
Jim van Vliet <jimvanvliet98@gmail.com>
2016-03-15 19:08:46 +0100

    asdasdasdasd

--- mainScript.js
+++ mainScript.js
@@ -23,7 +23,7 @@ window.addEventListener("load",function() { // Wait for the window to finish loa
     }
     );
 
-    var mouseListenerClass = new Q.Class.Evented();
+    var mouseListenerClass = new Q.Evented();
     mouseListenerClass.on("step", function()
     {
         Q.stage(void 0).centerOn(Q.inputs['mouseX'],Q.inputs['mouseY']);
