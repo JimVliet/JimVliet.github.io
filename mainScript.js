c93b7b6754db4ab1bf9ccf07366043b5217cff0c
JimVliet <jimvanvliet98@gmail.com>
2016-03-15 13:25:29 +0100

    Added small view

--- mainScript.js
+++ mainScript.js
@@ -10,6 +10,7 @@ window.addEventListener("load",function() { // Wait for the window to finish loa
     Q.scene("test_01", function(stage)
     {
     	Q.stageTMX("BEGINMAP.tmx", stage);
+    	stage.centerOn(200, 800)
     }
     );
     //wat leuks
