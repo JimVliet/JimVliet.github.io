e6ecdef2d5f57bf7b1e5004ffc468d8f3e679df5
Jim van Vliet <jimvanvliet98@gmail.com>
2016-03-18 19:31:27 +0100

    Added url checker

--- mainScript.js
+++ mainScript.js
@@ -1,31 +1,26 @@
+//The url link is used for checking if this project is running on a local server. If so it needs a different url
+var webUrl = window.location.href;
+var additionalPath = "";
+if (webUrl.indexOf("localhost") > -1)
+{
+    console.log("Running on localhost");
+    additionalPath = '/JimVliet.github.io';
+}
+
 window.addEventListener("load",function() { // Wait for the window to finish loading
- 
-	var Q = window.Q = Quintus({development: true, imagePath: "/sprites/Graphics/Autotiles/"})                // Create a new engine instance
+
+	var Q = window.Q = Quintus({development: true, imagePath: additionalPath + "/sprites/Graphics/Autotiles/"})                // Create a new engine instance
 	    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX") // Load any needed modules
 	    .setup("myGame", {maximize:true})                        // Bind Quintus to the canvas with ID "myGame"
-	    .controls()                             // Add in default controls (keyboard, buttons)
+	    .controls(true)                             // Add in default controls (keyboard, buttons)
 	    .touch();                               // Add in touch support (for the UI)
-    
+
     Q.input.mouseControls();
 
-    Q.Sprite.extend("listener", {
-        init: function(p) {
-            this._super(p, {
-                asset: "TestingTile.png"
-            });
-        },
-        step: function(dt)
-        {
-            console.log("X: " + Q.inputs['mouseX'] + " Y: " + Q.inputs['mouseY']);
-            Q.stage().moveTo(Q.inputs['mouseX'],Q.inputs['mouseY']);
-        }
-    });
 
     Q.scene("test_01", function(stage)
     {
     	Q.stageTMX("BEGINMAP.tmx", stage);
-    	stage.add("viewport");
-        stage.insert(new Q.listener());
     }
     );
     //wat leuks
